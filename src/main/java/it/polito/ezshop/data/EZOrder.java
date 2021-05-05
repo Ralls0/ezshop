34c23667aa64931001cae9f38f76daeb43694c30
coccocarmiano <s248992@studenti.polito.it>
2021-05-05 14:52:06 +0200

    Added EZOrder

---
+++ src/main/java/it/polito/ezshop/data/EZOrder.java
@@ -0,0 +1,75 @@
+package it.polito.ezshop.data;
+
+public class EZOrder implements Order {
+
+    private Integer balanceId;
+    private Integer orderId;
+    private Integer quantity;
+    private String productCode;
+    private String status;
+    private Double pricePerUnit;
+
+    @Override
+    public Integer getBalanceId() {
+        return balanceId;
+    }
+
+    @Override
+    public void setBalanceId(Integer balanceId) {
+        this.balanceId = balanceId;
+    }
+
+    @Override
+    public String getProductCode() {
+        return productCode;
+    }
+
+    @Override
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+        
+    }
+
+    @Override
+    public double getPricePerUnit() {
+        return pricePerUnit.doubleValue();
+    }
+
+    @Override
+    public void setPricePerUnit(double pricePerUnit) {
+        this.pricePerUnit = Double.valueOf(pricePerUnit);
+        
+    }
+
+    @Override
+    public int getQuantity() {
+        return quantity.intValue();
+    }
+
+    @Override
+    public void setQuantity(int quantity) {
+        quantity = Integer.valueOf(quantity);
+    }
+
+    @Override
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public void setStatus(String status) {
+        this.status = status;
+        
+    }
+
+    @Override
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    @Override
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+    
+}
