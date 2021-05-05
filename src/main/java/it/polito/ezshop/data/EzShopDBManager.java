5d0ad3e6ecf66aebca9258178286d7954ea71997
Davide Fersino <s245613@studenti.polito.it>
2021-05-05 23:56:32 +0200

    fixed db methods of user added db methods for customer class

--- src/main/java/it/polito/ezshop/data/EzShopDBManager.java
+++ src/main/java/it/polito/ezshop/data/EzShopDBManager.java
@@ -41,7 +41,7 @@ public class EzShopDBManager {
         List<User> users = new ArrayList<>();
         while (res.next()) {
             Integer id = res.getInt("ID");
-            String username = res.getString("Name");
+            String username = res.getString("Username");
             String password = res.getString("Password");
             String role = res.getString("Role");
 
@@ -80,7 +80,7 @@ public class EzShopDBManager {
     }
 
     public boolean saveUser(User user) throws SQLException {
-        PreparedStatement statement = db.prepareStatement("INSERT INTO Users (ID, Username, Password, Role) VALUES (?, ?, ?, ?)");
+        PreparedStatement statement = db.prepareStatement("INSERT INTO Users (ID, Username, Password, Role) VALUES (?, '?', '?', '?')");
         statement.setInt(1, user.getId());
         statement.setString(2, user.getUsername());
         statement.setString(3, user.getPassword());
@@ -89,18 +89,85 @@ public class EzShopDBManager {
         return statement.execute();
     }
 
-    public boolean updateUser(User user) throws SQLException {
-        PreparedStatement statement = db.prepareStatement("UPDATE Users SET Username = '?', Password = '?', Role = '?' WHERE ID = ?");
-        statement.setInt(4, user.getId());
-        statement.setString(1, user.getUsername());
-        statement.setString(2, user.getPassword());
-        statement.setString(3, user.getRole());
+    public boolean updateUserRights(Integer id, String role) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("UPDATE Users SET Role = '?' WHERE ID = ?");
+        statement.setInt(2, id);
+        statement.setString(1, role);
 
         return statement.execute();
     }
 
     public boolean deleteUser(Integer id) throws SQLException {
         PreparedStatement statement = db.prepareStatement("DELETE FROM Users WHERE ID = ?");
+        statement.setInt(1, id);
+
+        return statement.execute();
+    }
+
+    //      CUSTOMER CLASS QUERIES
+
+    public Integer getNextCustomerID() throws SQLException {
+        String sql = "SELECT MAX(ID) FROM Customers";
+        ResultSet res = db.executeSelectionQuery(sql);
+
+        if (res.next()) {
+            Integer nextID = res.getInt("ID") + 1;
+            return nextID;
+        }
+        return 1;
+    }
+
+    public List<Customer> loadAllCustomers() throws SQLException {
+        String sql = "SELECT * FROM Customers";
+        ResultSet res = db.executeSelectionQuery(sql);
+        List<Customer> customers = new ArrayList<>();
+        while (res.next()) {
+            Integer id = res.getInt("ID");
+            String name = res.getString("Name");
+            String card = res.getString("Card");
+            Integer points = res.getInt("Points");
+
+            EzCustomer customer = new EzCustomer(id, name, card, points);
+            customers.add(customer);
+        }
+
+        return customers;
+    }
+
+    public Customer loadCustomer(Integer id) throws SQLException {
+        String sql = "SELECT * FROM Customer WHERE ID = "+ id;
+        ResultSet res = db.executeSelectionQuery(sql);
+        if (res.next()) {
+            String name = res.getString("Name");
+            String card = res.getString("Card");
+            Integer points = res.getInt("Points");
+
+            return new EzCustomer(id, name, card, points);
+        }
+        return null;
+    }
+
+    public boolean saveCustomer(Customer customer) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("INSERT INTO Customers (ID, Name, Card, Points) VALUES (?, '?', '?', ?)");
+        statement.setInt(1, customer.getId());
+        statement.setString(2, customer.getCustomerName());
+        statement.setString(3, customer.getCustomerCard());
+        statement.setInt(4, customer.getPoints());
+
+        return statement.execute();
+    }
+
+    public boolean updateCustomer(Integer id, String newName, String newCard) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("UPDATE Customers SET Name = '?', Card = '?' WHERE ID = ?");
+        statement.setInt(4, id);
+        statement.setString(1, newName);
+        statement.setString(2, newCard);
+
+        return statement.execute();
+    }
+
+    public boolean deleteCustomer(Integer id) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("DELETE FROM Customers WHERE ID = ?");
         statement.setInt(1, id);
 
         return statement.execute();
