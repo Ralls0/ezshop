ffd38b0cadadfdd8a9c749e59bc6196bbcaa14d0
Davide Fersino <s245613@studenti.polito.it>
2021-05-05 23:44:57 +0200

    added table creation at app starting added db method for user class

--- src/main/java/it/polito/ezshop/EZShop.java
+++ src/main/java/it/polito/ezshop/EZShop.java
@@ -4,6 +4,7 @@ import it.polito.ezshop.data.EZShopInterface;
 import it.polito.ezshop.data.EzShopDBManager;
 import it.polito.ezshop.view.EZShopGUI;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -18,15 +19,46 @@ public class EZShop {
 
     public static void createTableIfNotExists(){
         try {
-            EzShopDBManager.getInstance().getConnector().execute("CREATE TABLE TEST (" +
-                    " Name varchar(50) NOT NULL," +
-                    " PRIMARY KEY(Name)" +
-                    ")");
+            EzShopDBManager.getInstance().getConnector().execute("CREATE TABLE Users (" +
+                                                                    " ID int NOT NULL," +
+                                                                    " Username varchar(255) UNIQUE," +
+                                                                    " Password varchar(255) NOT NULL," +
+                                                                    " Role varchar(255) NOT NULL," +
+                                                                    " PRIMARY KEY(ID)" +
+                                                                    ")");
 
-            EzShopDBManager.getInstance().getConnector().execute("INSERT INTO TEST" +
-                    " VALUES('aaa')");
+            EzShopDBManager.getInstance().getConnector().execute("CREATE TABLE Customers (" +
+                                                                    " ID int NOT NULL," +
+                                                                    " Name varchar(255) NOT NULL," +
+                                                                    " Card varchar(255) NOT NULL," +
+                                                                    " Points int NOT NULL," +
+                                                                    " PRIMARY KEY(ID)" +
+                                                                    ")");
+
+            EzShopDBManager.getInstance().getConnector().execute("CREATE TABLE Products (" +
+                                                                    " ID int NOT NULL," +
+                                                                    " ProductCode varchar(255) UNIQUE," +
+                                                                    " Description varchar(255) NOT NULL," +
+                                                                    " Quantity int NOT NULL," +
+                                                                    " PricePerUnit double NOT NULL," +
+                                                                    " Note varchar(255) NOT NULL," +
+                                                                    " Position varchar(255)," +
+                                                                    " PRIMARY KEY(ID)" +
+                                                                    ")");
+
+            EzShopDBManager.getInstance().getConnector().execute("CREATE TABLE BalanceOperations (" +
+                                                                    " ID int NOT NULL," +
+                                                                    " Description varchar(255) NOT NULL," +
+                                                                    " Amount double NOT NULL," +
+                                                                    " Date date NOT NULL," +
+                                                                    " PRIMARY KEY(ID)" +
+                                                                    ")");
         } catch (SQLException e) {
-            e.printStackTrace();
+            if (e.getSQLState().equals("X0Y32")) {              //      X0Y32 IS THE ERROR WHEN TABLE ALREADY EXISTS
+                System.out.println("Tables in DB already exists, skipping creation...");
+            } else {
+                e.printStackTrace();
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
--- src/main/java/it/polito/ezshop/data/EzShopDBManager.java
+++ src/main/java/it/polito/ezshop/data/EzShopDBManager.java
@@ -1,6 +1,10 @@
 package it.polito.ezshop.data;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EzShopDBManager {
 
@@ -16,6 +20,90 @@ public class EzShopDBManager {
             instance = new EzShopDBManager();
         }
         return instance;
+    }
+
+    //      USER CLASS QUERIES
+
+    public Integer getNextUserID() throws SQLException {
+        String sql = "SELECT MAX(ID) FROM Users";
+        ResultSet res = db.executeSelectionQuery(sql);
+
+        if (res.next()) {
+            Integer nextID = res.getInt("ID") + 1;
+            return nextID;
+        }
+        return 1;
+    }
+
+    public List<User> loadAllUsers() throws SQLException {
+        String sql = "SELECT * FROM Users";
+        ResultSet res = db.executeSelectionQuery(sql);
+        List<User> users = new ArrayList<>();
+        while (res.next()) {
+            Integer id = res.getInt("ID");
+            String username = res.getString("Name");
+            String password = res.getString("Password");
+            String role = res.getString("Role");
+
+            EzUser user = new EzUser(id, username, password, role);
+            users.add(user);
+        }
+
+        return users;
+    }
+
+    public User loadUser(String username, String password) throws SQLException {
+        String sql = "SELECT * FROM Users WHERE Username = '"+ username +"'";
+        ResultSet res = db.executeSelectionQuery(sql);
+        if (res.next()) {
+            String pass = res.getString("Password");
+            if (pass.equals(password)) {
+                Integer id = res.getInt("ID");
+                String role = res.getString("Role");
+                return new EzUser(id, username, password, role);
+            }
+        }
+        return null;
+    }
+
+    public User loadUser(Integer id) throws SQLException {
+        String sql = "SELECT * FROM Users WHERE ID = "+ id;
+        ResultSet res = db.executeSelectionQuery(sql);
+        if (res.next()) {
+            String username = res.getString("Username");
+            String password = res.getString("Password");
+            String role = res.getString("Role");
+
+            return new EzUser(id, username, password, role);
+        }
+        return null;
+    }
+
+    public boolean saveUser(User user) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("INSERT INTO Users (ID, Username, Password, Role) VALUES (?, ?, ?, ?)");
+        statement.setInt(1, user.getId());
+        statement.setString(2, user.getUsername());
+        statement.setString(3, user.getPassword());
+        statement.setString(4, user.getRole());
+
+        return statement.execute();
+    }
+
+    public boolean updateUser(User user) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("UPDATE Users SET Username = '?', Password = '?', Role = '?' WHERE ID = ?");
+        statement.setInt(4, user.getId());
+        statement.setString(1, user.getUsername());
+        statement.setString(2, user.getPassword());
+        statement.setString(3, user.getRole());
+
+        return statement.execute();
+    }
+
+    public boolean deleteUser(Integer id) throws SQLException {
+        PreparedStatement statement = db.prepareStatement("DELETE FROM Users WHERE ID = ?");
+        statement.setInt(1, id);
+
+        return statement.execute();
     }
 
     public DBConnector getConnector() {
