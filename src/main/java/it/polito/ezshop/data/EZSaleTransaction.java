f1845010a4af7e3a235b815d0fafc72fa752dfad
MARCO MANCO s281564 <s281564@studenti.polito.it>
2021-05-07 11:47:58 +0200

    feat: impelents SaleTransaction

--- src/main/java/it/polito/ezshop/data/EZSaleTransaction.java
+++ src/main/java/it/polito/ezshop/data/EZSaleTransaction.java
@@ -1,15 +1,24 @@
 package it.polito.ezshop.data;
 
-import java.util.List;
+import java.util.List; 
+import java.util.ArrayList; 
+import java.util.Iterator; 
 
 public class EZSaleTransaction implements SaleTransaction {
 
     private Integer id;
-    // private List<ProductQuantityAndDiscount> products;
-    private double cost;
+    private ArrayList<ProductQuantityAndDiscount> products;
     private String paymentType;
     private String status;
     private double discountRate;
+    private double price;
+
+    public EZSaleTransaction () {
+            this.id = 1; // TODO: SOSTITUIRE CON QUERY
+            this.products = new ArrayList<ProductQuantityAndDiscount>();
+            this.status = "open";
+            this.price = -1.0;
+        }
 
     public Integer getId() {
         return this.id;
@@ -19,24 +28,14 @@ public class EZSaleTransaction implements SaleTransaction {
         this.id = id;
     }
 
-    // public private getList<ProductQuantityAndDiscount>()
+    public ArrayList<ProductQuantityAndDiscount> getProducts() {
+		return this.products;
+	}
 
-    // {
-	// 	return this.List<ProductQuantityAndDiscount>;
-	// }
-
-    // public void setList<ProductQuantityAndDiscount>(private List<ProductQuantityAndDiscount>)
-    // {
-	// 	this.List<ProductQuantityAndDiscount> = List<ProductQuantityAndDiscount>;
-	// }
-
-    public double getCost() {
-        return this.cost;
-    }
-
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
+    public void setProducts(ArrayList<ProductQuantityAndDiscount> products)
+    {
+		this.products = products;
+	}
 
     public String getPaymentType() {
         return this.paymentType;
@@ -86,12 +85,129 @@ public class EZSaleTransaction implements SaleTransaction {
 
     @Override
     public double getPrice() {
-        return 0.0;
+        if (price != -1.0) {
+            return this.price;
+        }
+        else {
+            double sum = 0.0;
+            for(Iterator<ProductQuantityAndDiscount> i = products.iterator(); i.hasNext();) {
+                ProductQuantityAndDiscount p = i.next();
+                sum += (p.getProduct().getPricePerUnit()*p.getQuantity())-(p.getDiscountRate()/100*p.getProduct().getPricePerUnit()*p.getQuantity()); 
+            }
+            return sum-(sum*this.discountRate/100);
+        }
     }
 
     @Override
     public void setPrice(double price) {
-
+        this.price = price;
     }
+
+    public Integer computePoints() {
+        if (price != -1.0) {
+            return Integer.valueOf((int)(this.price / 10));
+        }
+        else {
+            return Integer.valueOf((int)(this.getPrice() / 10));
+        }
+    }
+
+    // TODO: return -1 cash is not enough
+    public double receiveCashPayment(double cash) {
+        double returnCash;
+        if (this.price != -1.0) {
+            returnCash = cash - this.price;
+        }
+        else {
+            returnCash = cash - this.getPrice();
+        }
+        this.status = "payed";
+        return returnCash;
+    }
+
+    // public boolean receiveCreditCardPayment(CreditCardCircuit circuit) {
+    //     return false;
+    // }
+
+    // TODO: diminuisci la quantità su scaffali. ProductType quantity?
+    public boolean addProductToSale(EzProductType product, int amount) {
+        
+            for(Iterator<ProductQuantityAndDiscount> i = products.iterator(); i.hasNext();) {
+                ProductQuantityAndDiscount p = i.next();
+                if(product.getBarCode().equals(p.getProduct().getBarCode())) {
+                    p.setQuantity(p.getQuantity()+Integer.valueOf(amount));
+                    return true;
+                }
+            }
+        try {
+            this.products.add(
+                new ProductQuantityAndDiscount(
+                    amount,
+                    0.0,
+                    product 
+                )
+            );
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e);
+            return false;
+        }
+
+        return true;
+    }
+
+    // TODO: aumenta la quantità su scaffali. ProductType quantity?
+    public boolean deleteProductFromSale(String productCode, int amount) {
+
+        for(Iterator<ProductQuantityAndDiscount> i = products.iterator(); i.hasNext();) {
+            ProductQuantityAndDiscount p = i.next();
+            if(productCode.equals(p.getProduct().getBarCode())) {
+                if(amount < p.getQuantity()) {
+                    p.setQuantity(p.getQuantity()-Integer.valueOf(amount));
+                    // controla che sia diminuito
+                    return true;
+                }
+                else if(amount == p.getQuantity()) {
+                    products.remove(p);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean applyDiscountRateToProduct(String productCode, double discountRate) {
+        
+        for(Iterator<ProductQuantityAndDiscount> i = products.iterator(); i.hasNext();) {
+            ProductQuantityAndDiscount p = i.next();
+            if(productCode.equals(String.valueOf(p.getProduct().getId()))) {
+                p.setDiscountRate(discountRate);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // TODO: db
+    public boolean endSaleTransaction() {
+    /**
+     * open, closed, payed  
+     */
+        if (this.status.equals("closed")) {
+            return false;
+        }
+        else {
+            this.status = "closed";
+            // TODO: scrivi su db ed eventuale return false  if there was a problem in registering the data
+            return true;
+        }
+    }
+
+
 
 }
